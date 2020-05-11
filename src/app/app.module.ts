dd94149de529f6eda5d1cd73a44e79869bec4be0
Monique13-rgb <61944084+Monique13-rgb@users.noreply.github.com>
2020-05-11 20:19:49 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebaseConfig : {
+    apiKey: "AIzaSyA0N-DIgqPuHQMOv2wPBnu8SGyyb-l9Fz0",
+    authDomain: "projetofinalloja.firebaseapp.com",
+    databaseURL: "https://projetofinalloja.firebaseio.com",
+    projectId: "projetofinalloja",
+    storageBucket: "projetofinalloja.appspot.com",
+    messagingSenderId: "793215521082",
+    appId: "1:793215521082:web:37d6de93caad6ea95cc003",
+    measurementId: "G-X3TCEVB4H7"
+  }
 };
 
 /*
